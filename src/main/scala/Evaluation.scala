2554364953950a54fc4914316d1b86acc60df514
Alexey Grachev <alexey.grachev@turbinekreuzberg.com>
2019-07-13 09:53:46 +0200

    WIP: adding ComplementaryPurchaseEvaluation object to Evaluation.scala; changing algorithm to precision at k like in recommender engine

--- src/main/scala/Evaluation.scala
+++ src/main/scala/Evaluation.scala
@@ -2,26 +2,46 @@ package org.template.complementarypurchase
 
 import org.apache.predictionio.controller.EngineParamsGenerator
 import org.apache.predictionio.controller.EngineParams
+// Usage:
+// $ pio eval org.template.complementarypurchase.ComplementaryPurchaseEvaluation \
+//   org.template.complementarypurchase.EngineParamsList
 
+case class PrecisionAtK(k: Int, ratingThreshold: Double = 2.0)
+    extends OptionAverageMetric[EmptyEvaluationInfo, Query, PredictedResult, ActualResult] {
+  require(k > 0, "k must be greater than 0")
+
+  override def header = s"Precision@K (k=$k, threshold=$ratingThreshold)"
+
+  override
+  def calculate(q: Query, p: PredictedResult, a: ActualResult): Option[Double] = {
+    val positives: Set[String] = a.ratings.filter(_.rating >= ratingThreshold).map(_.item).toSet
+
+    // If there is no positive results, Precision is undefined. We don't consider this case in the
+    // metrics, hence we return None.
+    if (positives.size == 0) {
+      None
+    } else {
+      val tpCount: Int = p.itemScores.take(k).filter(is => positives(is.item)).size
+      Some(tpCount.toDouble / math.min(k, positives.size))
+    }
+  }
+}
+object ComplementaryPurchaseEvaluation extends Evaluation {
+  engineEvaluator = (
+    RecommendationEngine(),
+    MetricEvaluator(
+      metric = PrecisionAtK(k = 10, ratingThreshold = 4.0),
+      otherMetrics = Seq(
+        PositiveCount(ratingThreshold = 4.0),
+        PrecisionAtK(k = 10, ratingThreshold = 2.0),
+        PositiveCount(ratingThreshold = 2.0),
+        PrecisionAtK(k = 10, ratingThreshold = 1.0),
+        PositiveCount(ratingThreshold = 1.0)
+      )))
+}
 trait BaseEngineParamsList extends EngineParamsGenerator {
   protected val baseEP = EngineParams(
     dataSourceParams = DataSourceParams(
       appName = "techselector_v3",
       evalParams = Some(DataSourceEvalParams(kFold = 5, queryNum = 10))))
-}
-
-case class Precision(label: Double)
-  extends OptionAverageMetric[EmptyEvaluationInfo, Query, PredictedResult, ActualResult] {
-  def calculate(query: Query, predicted: PredictedResult, actual: ActualResult)
-  : Option[Double] = {
-    if (predicted.label == label) {
-      if (predicted.label == actual.label) {
-        Some(1.0)  // True positive
-      } else {
-        Some(0.0)  // False positive
-      }
-    } else {
-      None  // Unrelated case for calcuating precision
-    }
-  }
 }
